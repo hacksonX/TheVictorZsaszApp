b31c21e31835ebcdf2e0038b88636e9831a0af17
hacksonX <mbhiza.shaun@gmail.com>
2016-04-06 19:51:20 +0200

    Initial upload of The project

---
+++ ProjectApp/static/js/ProjectApp/services/ProjectApp.service.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.authentication.services')
+    .factory('Authentication', Authentication);
+
+  Authentication.$inject = ['$cookies', '$http'];
+
+  function Authentication($cookies, $http) {
+    /**
+    * @name Authentication
+    * @desc The Factory to be returned
+    */
+    var Authentication = {
+      register: register
+    };
+
+    return Authentication;
+
+    function register(email, password, username) {
+      return $http.post('/api/v1/accounts/', {
+        username: username,
+        password: password,
+        email: email
+      });
+    }
+  }
+})();
